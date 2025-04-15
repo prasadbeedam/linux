b02e1387e07ed0d2f609fe885d82cbdaa4c8d7f2
prasad <prasad@gmail.com>
2025-04-15 11:07:42 +0530

    linux

--- diskutilization.sh
+++ diskutilization.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-echo "check the disk usage in linux system"
-disk_size= 'df -h|grep "/dev/xvda3"|awk '{print $5}'|cut -d '%' -f 1'
-echo "$disk_size% of disk is filled"
-if [ $disk_size -gt 50 ]; then
+echo "Checking disk usage on the system..."
 
-   echo "disk utilized more than 50%,expand disk or delete the files"
+# Get disk usage of /dev/xvda3 in percentage (without the % symbol)
+disk_size=$(df -h | grep "/dev/xvda3" | awk '{print $5}' | cut -d '%' -f 1)
+
+echo "$disk_size% of disk is filled."
+
+# Compare as integer
+if [ "$disk_size" -gt 50 ]; then
+    echo "Disk usage is above 50%. Consider cleaning up or expanding disk."
 else
-   echo "enough disk is available "
-fi   
+    echo "Enough disk space is available."
+fi 
