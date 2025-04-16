4ed079cbaa31cc1d010ee1a84253347eb7bc5835
prasad <prasad@gmail.com>
2025-04-16 11:36:16 +0530

    linux

---
+++ deletelogfiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "this script is to delete the old log files"
+path="$1"
+echo $path
+find $path -mtime +30 -delete
+if [ $? -gt 0 ]; then
+   echo "files are deleted succssfully"
+else
+   echo "having issue in filesystem or not able to find the files"
+fi      
